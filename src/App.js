01a7dd68052d31f09f50668e6b98d2cd5c145e02
Chewy <rkruzel576@gmail.com>
2021-06-13 15:06:45 -0400

    Bare bones example for a note app using EasyBase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { EasybaseProvider, useEasybase } from 'easybase-react';
+import { useEffect } from 'react';
+import ebconfig from './ebconfig'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
+      <EasybaseProvider ebconfig = {ebconfig}>
+        <Notes />
+        <NewNoteButton />
+      </EasybaseProvider>
     </div>
   );
+}
+
+function Notes() {
+  const { Frame, sync, configureFrame } = useEasybase();
+
+  useEffect(() => {
+    configureFrame({ tableName: "NOTES APP", limit: 10 });
+    sync();
+  }, []);
+
+  const noteRootStyle = {
+    border: "2px #0af solid",
+    borderRadius: 9,
+    margin: 20,
+    backgroundColor: "#efefef",
+    padding: 6
+  };
+
+  return (
+    <div style={{ width: 400 }}>
+      {Frame().map(ele => 
+        <div style={noteRootStyle}>
+          <h3>{ele.title}</h3>
+          <p>{ele.description}</p>
+          <small>{String(ele.createdate).slice(0, 10)}</small>
+        </div>
+      )}
+    </div>
+  )
+}
+
+function NewNoteButton() {
+  const { Frame, sync } = useEasybase();
+
+  const buttonStyle = {
+    position: "absolute",
+    left: 10,
+    top: 10,
+    fontSize: 21
+  }
+
+  const handleClick = () => {
+    const newTitle = prompt("Please enter a title for your note");
+    const newDescription = prompt("Please enter your description");
+    
+    Frame().push({
+      title: newTitle,
+      description: newDescription,
+      createdate: new Date().toISOString()
+    })
+    
+    sync();
+  }
+
+  return <button style={buttonStyle} onClick={handleClick}>📓 Add Note 📓</button>
 }
 
 export default App;
